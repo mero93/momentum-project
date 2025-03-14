308f6b5d335372e897a2131b6a7ed3764c4d2422
mero93 <merabkopaleishvili1993@gmail.com>
2025-03-14 23:25:40 +0400

    created pages for tasks and task details, set up routing, fixed scss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { TasksPageComponent } from './components/tasks-page/tasks-page.component';
+import { TaskDetailsComponent } from './components/task-details/task-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'tasks', component: TasksPageComponent },
+  { path: 'tasks/:id', component: TaskDetailsComponent },
+  { path: '', redirectTo: '/tasks', pathMatch: 'full' },
+  { path: '**', redirectTo: '/tasks' },
+];
